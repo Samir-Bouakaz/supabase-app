12a211ad800c3ac3bca134682fdd8af49373bcfa
Samir Bouakaz <enterprise.development.project@gmail.com>
2025-01-15 19:59:40 +0100

    Tri alphabétique des menus et sous-menus

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -107,25 +107,11 @@ export default function Navbar() {
             <div className="absolute right-0 mt-4 w-64 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-all duration-200 delay-100 group-hover:delay-0 p-2">
                 <div className="p-2">
                   <Link
-                    href="/gestion-securite/rapport-securite"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <ClipboardList className="h-4 w-4" />
-                    <span>Rapport de sécurité</span>
-                  </Link>
-                  <Link
                     href="/gestion-securite/controles-periodiques"
                     className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
                   >
                     <Wrench className="h-4 w-4" />
                     <span>Contrôles périodiques</span>
-                  </Link>
-                  <Link
-                    href="/gestion-securite/suivi-equipements"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <ShieldCheck className="h-4 w-4" />
-                    <span>Suivi des équipements</span>
                   </Link>
                   <Link
                     href="/gestion-securite/creation-tickets"
@@ -140,6 +126,63 @@ export default function Navbar() {
                   >
                     <Calendar className="h-4 w-4" />
                     <span>Planification des congés</span>
+                  </Link>
+                  <Link
+                    href="/gestion-securite/rapport-securite"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <ClipboardList className="h-4 w-4" />
+                    <span>Rapport de sécurité</span>
+                  </Link>
+                  <Link
+                    href="/gestion-securite/suivi-equipements"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <ShieldCheck className="h-4 w-4" />
+                    <span>Suivi des équipements</span>
+                  </Link>
+                </div>
+              </div>
+          </div>
+
+          {/* Menu Pilotage et Analyses */}
+          <div className="relative group">
+            <button
+              className="flex items-center space-x-2 text-white hover:text-[#35BDB6] transition-colors"
+            >
+              <LineChart className="h-5 w-5" />
+              <span>Pilotage et analyses</span>
+            </button>
+
+            <div className="absolute right-0 mt-4 w-64 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-all duration-200 delay-100 group-hover:delay-0 p-2 z-50">
+                <div className="p-2">
+                  <Link
+                    href="/pilotage/alertes"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <Bell className="h-4 w-4" />
+                    <span>Alertes</span>
+                  </Link>
+                  <Link
+                    href="/pilotage/rapports"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <FileText className="h-4 w-4" />
+                    <span>Rapports</span>
+                  </Link>
+                  <Link
+                    href="/pilotage/statistiques"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <BarChart className="h-4 w-4" />
+                    <span>Statistiques</span>
+                  </Link>
+                  <Link
+                    href="/pilotage/tableau-de-bord"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <LayoutDashboard className="h-4 w-4" />
+                    <span>Tableau de bord</span>
                   </Link>
                 </div>
               </div>
@@ -156,6 +199,13 @@ export default function Navbar() {
 
             <div className="absolute right-0 mt-4 w-64 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-all duration-200 delay-100 group-hover:delay-0 p-2">
                 <div className="p-2">
+                  <Link
+                    href="/referentiel/agents-prestataire"
+                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
+                  >
+                    <UserIcon className="h-4 w-4" />
+                    <span>Agents prestataire de sécurité</span>
+                  </Link>
                   <Link
                     href="/referentiel/personnel-bar"
                     className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
@@ -176,56 +226,6 @@ export default function Navbar() {
                   >
                     <Building className="h-4 w-4" />
                     <span>Société prestataire de sécurité</span>
-                  </Link>
-                  <Link
-                    href="/referentiel/agents-prestataire"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <UserIcon className="h-4 w-4" />
-                    <span>Agents prestataire de sécurité</span>
-                  </Link>
-                </div>
-              </div>
-          </div>
-
-          {/* Menu Pilotage et Analyses */}
-          <div className="relative group">
-            <button
-              className="flex items-center space-x-2 text-white hover:text-[#35BDB6] transition-colors"
-            >
-              <LineChart className="h-5 w-5" />
-              <span>Pilotage et analyses</span>
-            </button>
-
-            <div className="absolute right-0 mt-4 w-64 bg-white rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-all duration-200 delay-100 group-hover:delay-0 p-2 z-50">
-                <div className="p-2">
-                  <Link
-                    href="/pilotage/tableau-de-bord"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <LayoutDashboard className="h-4 w-4" />
-                    <span>Tableau de bord</span>
-                  </Link>
-                  <Link
-                    href="/pilotage/statistiques"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <BarChart className="h-4 w-4" />
-                    <span>Statistiques</span>
-                  </Link>
-                  <Link
-                    href="/pilotage/rapports"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <FileText className="h-4 w-4" />
-                    <span>Rapports</span>
-                  </Link>
-                  <Link
-                    href="/pilotage/alertes"
-                    className="flex items-center space-x-2 p-2 text-gray-700 hover:bg-[#35BDB6] hover:text-white rounded-md transition-colors"
-                  >
-                    <Bell className="h-4 w-4" />
-                    <span>Alertes</span>
                   </Link>
                 </div>
               </div>
